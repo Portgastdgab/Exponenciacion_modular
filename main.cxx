d8f6b5b62db38944e7a1fcef311aebefcb375684
Marcus Mors <alivezeh@gmail.com>
2021-06-10 18:57:14 -0500

    to stich, squiusmy, haven't tried it yet

--- main.cxx
+++ main.cxx
@@ -20,16 +20,30 @@ You should have received a copy of the GNU General Public License
 along with Exponenciacion_modular.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+// A simple program that computes the square root of a number
+#include <cmath>
 #include <iostream>
-#include <NTL/ZZ.h>
-// #include "include/modular_exponentiation/modular_exponentiation.hpp"
-#include "include/modular_exponentiation/power_module.hpp"
-// #include "power_module.hh"
+#include <string>
 
-using namespace std;
+#include "TutorialConfig.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-	cout << "Hello world!" << endl;
+	if (argc < 2)
+	{
+		// report version
+		std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+				  << Tutorial_VERSION_MINOR << std::endl;
+		std::cout << "Usage: " << argv[0] << " number" << std::endl;
+		return 1;
+	}
+
+	// convert input to double
+	const double inputValue = std::stod(argv[1]);
+
+	// calculate square root
+	const double outputValue = sqrt(inputValue);
+	std::cout << "The square root of " << inputValue << " is " << outputValue
+			  << std::endl;
 	return 0;
 }
